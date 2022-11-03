e198448372cbcdef90b40e99c9ba8e5939d45cc0
gui16778 <117320708+gui16778@users.noreply.github.com>
2022-11-03 16:06:40 +0800

    Create JS.myJS.js

---
+++ JS.myJS.js
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html>
+  <head> 				
+     <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous"> 
+    			  				
+    <meta charset="utf-8">
+    <meta name="description" content="Affordable and professional web design">
+	  <meta name="keywords" content="web design, affordable web design, professional web design">
+  	<meta name="author" content="Brad Traversy">
+    <title>	  <li class="current"><a href="services.html">Services</a></a></li></center></br>Guille's | Rabbitry </title>
+    <link rel="stylesheet" href="./css/style.css">
+    <link rel="icon" href="/storage/emulated/0/Pictures/Types-of-Rabbits.jpg">
+  </head>
+<body>
+     
+	  <ul class="current"><a href="Website Home.html"><h1>Home</h1></a></a></br>
+  <ul class="current"><a href="websiteHome.html"
+
+<ul class="current"><a href="About.html"><h1>About</h1></a></a></br>
+<ul class="current"><a href="Rabbit.html"
+
+<ul class="current"><a href="Rabbit.html"><h1>Services</h1></a></a></li></br>
+<ul class="current"><a href="Rabbit.html"
+
+			  
+	  <ul class="current"><a href="Contact.html"><h1>Contact</h1></a></a></li></br>
+  <ul class="current"><a href="Contact.html"
+
+		  <ul class="current"><a href="Terms And Privacy.html"><h1>Terms And Privacy</h1></a></a></</br>
+  <ul class="current"><a href="Terms And Privacy.html"
+			  			  			  			  			  		  			  			  			 <br><br> 			  			 			  			  			  				  			  			  <sectionid="showcase"
+       <div class="container" 
+       <div class="box"  
+       </section>  				
+       <ul><center><h1><i><font color="Black">Affordable Petype</font></h1></i></center></ul>
+       <section id="newsletter"></section>
+       				<div class="container"></div>
+       				<div class="box"></div>      							
+       <body background="/storage/emulated/0/Pictures/AdobeStock_233302249.jpeg"
+      width="10" height="5"
+       
+  
+      </div>
+    </section>
+    <section id="boxes">
+      <div class="container">
+      		<div class="box"
+        <h1></h1>
+        <form>
+        <h1><center><br><br><input type="email" placeholder="Enter Email..." required></br></br></center></h1>
+        
+          <center><h1><i><button type="submit" class="button_">Subscribe</button></i></center>   
+           </form>
+  <footer>			
+ <br><center><h1><p>Guille's Rabbitry</p></h1></center><br>
+    </footer>
+  </body>
+</html>
